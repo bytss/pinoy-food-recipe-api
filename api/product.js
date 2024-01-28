c6a18675d82cded3c0e7835ca544c5513743eaf8
jalaiza <bytess1010@gmail.com>
2024-01-29 00:08:56 +0800

    init

--- api/product.js
+++ api/product.js
@@ -11,13 +11,18 @@ const recipesData = JSON.parse(jsonData);
 const recipes = recipesData.recipes;
 
 // Define a route to get a random recipe
-router.get("/random", (req, res) => {
-  // Generate a random index within the range of recipes array
-  const randomIndex = Math.floor(Math.random() * recipes.length);
-  // Get the random recipe
-  const randomRecipe = recipes[randomIndex];
-  // Send the random recipe as JSON response
-  res.json(randomRecipe);
+router.get("/random", async (req, res) => {
+  try {
+    // Generate a random index within the range of recipes array
+    const randomIndex = Math.floor(Math.random() * recipes.length);
+    // Get the random recipe
+    const randomRecipe = recipes[randomIndex];
+    // Send the random recipe as JSON response
+    res.json(randomRecipe);
+  } catch (error) {
+    console.error("Error in /random route:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 /**
