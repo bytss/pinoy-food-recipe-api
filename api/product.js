11a0681598b58aa636bafa2dbff022b87632fd48
jalaiza <bytess1010@gmail.com>
2024-01-29 00:02:12 +0800

    initial

--- api/product.js
+++ api/product.js
@@ -1,29 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-// Read the JSON file
-const jsonData = fs.readFileSync("recipes_data.json");
-const recipesData = JSON.parse(jsonData);
-
-// Extract the recipes array from the JSON data
-const recipes = recipesData.recipes;
-
 /**
  * GET product list.
  *
  * @return product list | empty.
  */
-
-// Define a route to get a random recipe
-router.get("/random", (req, res) => {
-  // Generate a random index within the range of recipes array
-  const randomIndex = Math.floor(Math.random() * recipes.length);
-  // Get the random recipe
-  const randomRecipe = recipes[randomIndex];
-  // Send the random recipe as JSON response
-  res.json(randomRecipe);
-});
-
 router.get("/", async (req, res) => {
   try {
     res.json({
