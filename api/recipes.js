192a8d4703aac6a91bc02a7eadc71ba1da560fb2
jalaiza <bytess1010@gmail.com>
2024-01-30 14:31:44 +0800

    init

--- api/recipes.js
+++ api/recipes.js
@@ -52,4 +52,48 @@ router.get("/random", (req, res) => {
   });
 });
 
+// Define a route to get recipes based on a search query
+router.get("/search=:query", (req, res) => {
+  // Construct the file path
+  const filePath = path.resolve(__dirname, "recipes_data.json");
+
+  // Asynchronously read the JSON file
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      console.error("Error reading file:", err);
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
+
+    try {
+      // Parse the JSON data
+      const recipesData = JSON.parse(data);
+      const recipes = recipesData.recipes;
+
+      // Get the search query from the URL parameter
+      const searchTerm = req.params.query;
+
+      // If no search query provided, return all recipes
+      if (!searchTerm) {
+        return res.json(recipes);
+      }
+
+      // Filter recipes based on the search query
+      const filteredRecipes = recipes.filter((recipe) =>
+        recipe.name.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+
+      // If no recipes match the search query, return a 404 response
+      if (filteredRecipes.length === 0) {
+        return res.status(404).json({ message: "No recipes found" });
+      }
+
+      // Send the filtered recipes as JSON response
+      res.json(filteredRecipes);
+    } catch (error) {
+      console.error("Error parsing JSON:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  });
+});
+
 module.exports = router;
